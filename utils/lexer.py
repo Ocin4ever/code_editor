ed2bd75f1fa9800c2ba8042d55dfbd290c21c34d
Ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2026-05-20 13:02:43 +0200

    utils: lexer.py: optimize regex compilation
    Previously regex where recompiled for every words as it was in the "while pos < len(text):" loop
    This is obviously useless and takes resources

--- utils/lexer.py
+++ utils/lexer.py
@@ -5,16 +5,17 @@ from .tokens import TOKEN_TYPES, Token
 
 class Lexer:
     def __init__(self):
-        self.rules = TOKEN_TYPES
+        self.rules = [
+            (token_type, re.compile(regex)) for token_type, regex in TOKEN_TYPES
+        ]
 
     def tokenize(self, text):
         tokens = []
         pos = 0
         while pos < len(text):
             match = None
-            for token_type, pattern in self.rules:
-                regex = re.compile(pattern)
-                match = regex.match(text, pos)
+            for token_type, compiled_regex in self.rules:
+                match = compiled_regex.match(text, pos)
                 if match:
                     if token_type not in ["WHITESPACE", "COMMENT"]:
                         tokens.append(Token(token_type, match.group(0)))
